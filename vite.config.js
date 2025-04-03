c88c34ec5076881658a2932c28ad6a5fa8dfe3dc
LuisLara28 <luis_28_92lara@hotmail.com>
2025-04-02 18:42:54 -0600

    fix: patch crypto.getRandomValues for Netlify build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// 👇👇 Polyfill de Web Crypto para evitar errores en Netlify
+import { Crypto } from "@peculiar/webcrypto";
+globalThis.crypto = new Crypto();
+
 export default defineConfig({
 	plugins: [react()],
-	optimizeDeps: {
-		exclude: ["crypto"],
-	},
 });
