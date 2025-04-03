4a78ac9ed3272657d4b0af45ad67082965c0da45
LuisLara28 <luis_28_92lara@hotmail.com>
2025-04-02 18:50:15 -0600

    fix: downgrade vite, publish to dist, clean build for Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// 👇👇 Polyfill de Web Crypto para evitar errores en Netlify
-import { Crypto } from "@peculiar/webcrypto";
-globalThis.crypto = new Crypto();
-
 export default defineConfig({
 	plugins: [react()],
 });
